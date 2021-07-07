4c4e5da788ba50c841462c2aae428d1231544534
Nikhil Gaur <31702600+NikhilGaur406@users.noreply.github.com>
2021-07-07 16:22:06 -0500

    Successfully added view 1

--- mysite/main/urls.py
+++ mysite/main/urls.py
@@ -5,5 +5,6 @@ from . import views # from current package import views module
 
 urlpatterns = [
     path("", views.index, name="index"), # home page, go to views.index, has name index, go to that page renders tech with tim header 1
-    path("home/", views.index, name="index")
+    path('v1/', views.v1, name="view 1")
+    #path("home/", views.index, name="index")
 ]
