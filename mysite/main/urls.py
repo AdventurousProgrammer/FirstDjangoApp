2de701c326a62d73015369d43d10751051c3c757
Nikhil Gaur <31702600+NikhilGaur406@users.noreply.github.com>
2021-07-07 19:03:47 -0500

    registered todolist database with admins page

--- mysite/main/urls.py
+++ mysite/main/urls.py
@@ -4,6 +4,6 @@ from django.urls import path
 from . import views # from current package import views module
 
 urlpatterns = [
-    path("", views.index, name="index") # home page, go to views.index, has name index, go to that page renders tech with tim header 1
+    path("<int:id>", views.index, name="index") # home page, go to views.index, has name index, go to that page renders tech with tim header 1
     #path("home/", views.index, name="index")
 ]
--- mysite/main/views.py
+++ mysite/main/views.py
@@ -1,10 +1,14 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import ToDoList, Item
+
 # Create your views here.
 
 
-def index(response):
-    return HttpResponse("<h1>tech with tim!</h1>")
+def index(response, id):
+    t = ToDoList.objects.get(id=id)
+    item = t.item_set.get(id=1)
+    return HttpResponse(f"<h1>tech with tim! Looking at list: {t.name} it has {item.text}</h1>")
 
 def v1(response):
     return HttpResponse("<h1>View 1!</h1>")
