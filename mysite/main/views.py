bd278b75db04ebe197b33934cfc8d867efce8141
Nikhil Gaur <31702600+NikhilGaur406@users.noreply.github.com>
2021-07-26 18:45:31 -0500

    Added ability to add item to a list

--- mysite/main/views.py
+++ mysite/main/views.py
@@ -4,7 +4,27 @@ from .models import ToDoList, Item
 from .forms import CreateNewList
 # Create your views here.
 def index(response, id):
+    print(f'Entering Index View')
     t = ToDoList.objects.get(id=id)
+    print(type(response))
+    print(response == HttpResponse)
+    if response.method == 'POST':
+        if response.POST.get("save"):
+            print(f'response object type: {type(response)}')
+            for item in t.item_set.all():
+                if response.POST.get("c" + str(item.id)) == "clicked":
+                    print(f'Saved Item: {item.id}')
+                    item.complete = True
+                else:
+                    item.complete = False
+        elif response.POST.get("newItem"): # custom form so cannot do is valid
+            text = response.POST.get("new")
+            if len(text) > 2:
+                t.item_set.create(text=text, complete=False)
+            else:
+                print('Invalid Input')
+            pass
+
     return render(response, "main/list.html", {"ls":t}) # assuming already in templates folder?
 
 def home(response):
